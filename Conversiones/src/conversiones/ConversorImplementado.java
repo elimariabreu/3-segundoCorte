f8e1bdecad558941fbbbe94759365878c8e06601
Estudiantes <Estudiantes@labing50408.udistrital.edu.co>
2017-10-13 16:55:00 -0500

    CLASE IMPLEMENTADA

---
+++ Conversiones/src/conversiones/ConversorImplementado.java
@@ -0,0 +1,19 @@
+
+package conversiones;
+
+
+public class ConversorImplementado implements ITransformador{
+    double dato;
+
+    @Override
+    public double grado2radian() {
+        
+    }
+
+    @Override
+    public double kelvin2celsius() {
+        
+    }
+    
+    
+}
